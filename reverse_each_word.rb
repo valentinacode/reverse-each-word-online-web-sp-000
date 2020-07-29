dd914e09ed0470accb71f7799d1c33664bbd1441
Valentina E <tiyahelfrih@gmail.com>
2020-07-29 16:30:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
 def reverse_each_word(sentence1)
   array = []
-  sentence1.each do |word|
-    sentence1.reverse
-    "#{word}"
-end
-
-def reverse_each_word(sentence1)
-  array = []
  sentence = "Hello there, and how are you?"
  sentence.each {|name| name.reverse}
+end
 end
