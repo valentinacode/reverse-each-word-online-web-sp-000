f84bc0e50b148c84eef8926e06dfeeeafac94a9c
Valentina E <tiyahelfrih@gmail.com>
2020-07-29 16:38:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,3 @@
-def reverse_each_word(sentence)
-  words_array = sentence.split(" ")
-  new_array = []
-  new_array = words_array.collect {|word| word.reverse }
-  new_array.join(" ")
+ def reverse_each_word(sentence)
+sentence.reverse.split.reverse.join(" ")
 end
